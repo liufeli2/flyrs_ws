9f12deec37114f7a104edc4f3325b23bec6fcada
lukewarmtemp <luke.jia.yang@gmail.com>
2025-02-28 11:23:37 -0500

    added parsight repo to src

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd src
-git clone https://github.com/lukewarmtemp/fl2.git
-git clone https://github.com/lukewarmtemp/px4_autonomy_modules.git
-git clone https://github.com/lukewarmtemp/fl3.git
+git clone git@github.com:lukewarmtempfl2.git
+git clone git@github.com:lukewarmtemp/px4_autonomy_modules.git
+git clone git@github.com:lukewarmtemp/fl3.git
+git clone git@github.com:lukewarmtemp/ParSight.git
