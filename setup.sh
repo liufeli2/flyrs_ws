5f29e9b76dde15d90c5b697a3632fddae10be26a
lukewarmtemp <luke.jia.yang@gmail.com>
2025-03-04 13:58:44 -0500

    add gui

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ git clone git@github.com:lukewarmtempfl2.git
 git clone git@github.com:lukewarmtemp/px4_autonomy_modules.git
 git clone git@github.com:lukewarmtemp/fl3.git
 git clone git@github.com:lukewarmtemp/ParSight.git
-git clone git@github.com:lukewarmtemp/drone-gui.git
+git clone git@github.com:lukewarmtemp/drone_gui.git
